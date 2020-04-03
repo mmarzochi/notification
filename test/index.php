8f70b2e7d8e661ac4c433a05dcafdccc88a661c5
Matheus Marzochi <matheus.ace@hotmail.com>
2020-04-02 22:23:08 -0300

    Test fix

--- test/index.php
+++ test/index.php
@@ -4,7 +4,9 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "smtp.office365.com", "test@test.com", "test123", "TLS", '587', "matheus.ace@hotmail.com", "Matheus Web");
-$novoEmail->sendMail("Assunto de Teste", "<p>Esse é um e-mail de <b>teste</b>!</p>", "matheus.ace@hotmail.com", "Matheus Marzochi", "matheus.ace@hotmail.com", "Matheus Marzochi");
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "smtp secure (tls / ssl)", "port (587)",
+    "from@email.com", "From Name");
+
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
 
 var_dump($novoEmail);
